9c65bdba9255364c8069fd0fef8d1147d9bc8402
rufael <rufaelkidanemariamyohannes@Rufaels-MBP.attlocal.net>
2020-05-27 14:16:46 -0400

    some tests

--- ReactiveMongoExample/src/test/java/com/reactiveDemo/model/DepartmentTest.java
+++ ReactiveMongoExample/src/test/java/com/reactiveDemo/model/DepartmentTest.java
@@ -2,36 +2,57 @@ package com.reactiveDemo.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class DepartmentTest {
 
+    Employee employee= new Employee();
+    Department department = new Department();
+    List<Employee> employees = new ArrayList<>();
     @BeforeEach
     void setUp() {
+        employee.setFirstName("Rufael");
+        employee.setLastName("kidanemariam");
+        employees.add(employee);
+        department.setName("Finance");
+        department.setEmployees(employees);
     }
-
     @Test
     void getId() {
-    }
+        assertNull(department.getId());
+        }
 
     @Test
     void getName() {
+        assertEquals(department.getName(),"Finance");
     }
 
     @Test
     void getEmployees() {
+        assertEquals(department.getEmployees(),employees);
     }
 
     @Test
     void setId() {
+        department.setId("1");
+        assertEquals(department.getId(),"1");
     }
 
     @Test
     void setName() {
+        department.setName("Commerce");
+
+        assertEquals(department.getName(),"Commerce");
     }
 
     @Test
     void setEmployees() {
+        department.setEmployees(null);
+        assertNull(department.getEmployees());
     }
 }
--- ReactiveMongoExample/src/test/java/com/reactiveDemo/model/EmployeeTest.java
+++ ReactiveMongoExample/src/test/java/com/reactiveDemo/model/EmployeeTest.java
@@ -2,36 +2,50 @@ package com.reactiveDemo.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 class EmployeeTest {
 
+    Employee employee = new Employee();
     @BeforeEach
     void setUp() {
+        employee.setFirstName("Rufael");
+        employee.setLastName("Yohannes");
     }
 
     @Test
     void getId() {
+        assertNull(employee.getId());
     }
 
     @Test
     void getFirstName() {
+        assertEquals(employee.getFirstName(),"Rufael");
     }
 
     @Test
     void getLastName() {
+        assertEquals(employee.getLastName(),"Yohannes");
     }
 
     @Test
     void setId() {
+        employee.setId("1");
+        assertEquals(employee.getId(),"1");
     }
 
     @Test
     void setFirstName() {
+        employee.setFirstName("Kidanemariam");
+        assertEquals(employee.getFirstName(),"Kidanemariam");
     }
 
     @Test
     void setLastName() {
+        assertNotEquals(employee.getLastName(),"Kidanemariam");
     }
 }
